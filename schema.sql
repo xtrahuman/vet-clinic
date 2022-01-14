ce5c9054ff9ba9e0c50661e9d1e8921ed4d551a0
unknown <okpara_tochukwu@yahoo.com>
2022-01-14 12:39:49 +0100

    created table vets, created join table visits and speciallization, inserted data, queried data

--- schema.sql
+++ schema.sql
@@ -64,3 +64,36 @@ ADD COLUMN owners_id INT;
 
 ALTER TABLE animals 
 ADD FOREIGN KEY(owners_id) REFERENCES owners(id) ON DELETE CASCADE;
+
+
+
+CREATE TABLE vets (
+    id BIGSERIAL PRIMARY KEY,
+    name varchar(100),
+    age int,
+    date_of_graduation date
+);
+
+
+CREATE TABLE specializations (
+    species_id int,
+    vets_id int,
+    CONSTRAINT fk_species 
+     FOREIGN KEY(species_id) 
+      REFERENCES species(id),
+    CONSTRAINT fk_vets 
+     FOREIGN KEY(vets_id) 
+      REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    animals_id int,
+    vets_id int,
+    date_of_visit date,
+    CONSTRAINT fk_animals 
+     FOREIGN KEY(animals_id) 
+       REFERENCES animals(id),
+    CONSTRAINT fk_vets 
+     FOREIGN KEY(vets_id) 
+       REFERENCES vets(id)
+);
